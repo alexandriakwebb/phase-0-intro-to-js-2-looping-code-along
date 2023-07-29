a1539f38d1786cabbf54d6f63ff6f1467e1f053e
Alexandria Webb <alexandriakwebb@gmail.com>
2023-07-29 16:32:36 -0600

    creates tests that pass.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards (nameArray, eventName) {
+    let thankYouMessage = [];
+    for (let i = 0; i < nameArray.length; i++) {
+        thankYouMessage.push(`Thank you, ${nameArray[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return thankYouMessage;
+}
+
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown (number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+
+}
